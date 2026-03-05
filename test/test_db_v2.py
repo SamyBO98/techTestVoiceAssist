99276d83b3126892ad0d66135df7ee67fac85771
SamyBO98 <90256223+SamyBO98@users.noreply.github.com>
2026-03-05 19:59:27 +0100

    remove useless import

--- test/test_db_v2.py
+++ test/test_db_v2.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from app import app
-from db import db, CallInfo
+from db import CallInfo
 with app.app_context():
     calls = CallInfo.query.all()
     for c in calls:
