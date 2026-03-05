7c9772188a130c7420d346f721c953fca9ef1fd2
SamyBO98 <90256223+SamyBO98@users.noreply.github.com>
2026-03-05 11:39:28 +0100

    Change DB init (from sqlite3 to SQLAlchemy)

--- db.py
+++ db.py
@@ -1,38 +1,32 @@
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
-#Name of the DB
-DB_NAME = "voiceAssist.db"
+# ---------------------------------------------------------------------------------
+# DB CONNECTION NOTE
+#
+# For simple tests or low usage, opening and closing a session per script run 
+# works fine. In a web app (e.g., Flask), it's better to use one session per 
+# HTTP request or a connection pool. SQLAlchemy manages this automatically in Flask.
+# ---------------------------------------------------------------------------------
 
+db = SQLAlchemy()
+TABLE_NAME = "calls_info"
 
-#DB initialization
-def init_db():
-    #Connect to DB (if file doesnt exist it will be automatically created by SQLite)
-    conn = sqlite3.connect(DB_NAME)
-    #Allow us to exec command
-    c = conn.cursor()
-    #Create the table call_end_info
-    #ID primary key that increments each time
-    #Appointment date in Text (no Date type in SQLite)
-    c.execute("""
-        CREATE TABLE IF NOT EXISTS call_end_info (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            appointment_date TEXT
-        )
-    """)
-    #Validate all changes
-    conn.commit()
-    #Close DB connexion
-    conn.close()
+class CallInfo(db.Model):
+    __tablename__ = TABLE_NAME
+    #ID that will increment automatically (Integer type + our primary key)
+    id = db.Column(db.Integer, primary_key=True)
+    #index=True -> can search by call_id faster
+    call_id = db.Column(db.String(128), index=True)
+    appointment_date = db.Column(db.String(128), nullable=False)
+    creation_date = db.Column(
+        db.DateTime,
+        default=lambda: datetime.now(ZoneInfo("Europe/Paris")),
+        nullable=False
+    )
+    #db.Column(db.Integer, default=lambda: int(datetime.now(ZoneInfo("Europe/Paris")).timestamp()))
 
-#Update DB (Insert)
-def save_appointment(appointment_date: str):
-    #Connexion to our DB
-    conn = sqlite3.connect(DB_NAME)
-    #Allow us to exec command
-    c = conn.cursor()
-    #Insert in our table (more secure with placeholder)
-    c.execute("INSERT INTO call_end_info (appointment_date) VALUES (?)", (appointment_date,))
-    #Validate all changes
-    conn.commit()
-    #Close DB connexion
-    conn.close()
+    #return a printable representation of the object
+    def __repr__(self) -> str:
+        return ("id= " + str(self.id) + " | call_id= " + str(self.call_id) +  " | appointment_date= " + str(self.appointment_date)+ " | creation_date= " + str(self.creation_date))
---
+++ test_db/test_db.py
@@ -0,0 +1,30 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+from db import CallInfo
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+#Create DB
+DB_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "calls_info.db"))
+engine = create_engine(f"sqlite:///{DB_FILE}", echo=True)
+
+#Create table if doesnt exist
+CallInfo.metadata.create_all(engine)
+
+#Create a session to make tests
+Session = sessionmaker(bind=engine)
+
+#Create session that closes automatically
+with Session() as session:
+    # Add test
+    new_call = CallInfo(call_id="testIDDDDDDDDD", appointment_date="2026-03-06")
+    session.add(new_call)
+    session.commit()
+
+    # Print all results
+    all_calls = session.query(CallInfo).all()
+    for call in all_calls:
+        print(call)
