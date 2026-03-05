f7d78b6a1cdfe9e2ce7652028180de1ebb5f1907
SamyBO98 <90256223+SamyBO98@users.noreply.github.com>
2026-03-05 11:51:05 +0100

    DB Creation in app.py (flask)

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, request, jsonify
+from db import db, CallInfo
+from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
+
+DB_FILE = "calls_info.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_FILE}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 
 # Endpoint POST minimal pour /end-of-call
 @app.route("/end-of-call", methods=["POST"])
