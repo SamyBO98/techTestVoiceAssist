599d29ec0b13b1985673989c44812aed30157c29
Samy <90256223+SamyBO98@users.noreply.github.com>
2026-03-06 12:21:53 +0100

    Update test_db_v2.py

--- test/test_db_v2.py
+++ test/test_db_v2.py
@@ -1,10 +1,11 @@
 import sys
 import os
-from app import app
-from db import CallInfo
 
 # Add parent directory to path to import app and db modules
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import app
+from db import CallInfo
 
 # Print all records stored in the database
 with app.app_context():
